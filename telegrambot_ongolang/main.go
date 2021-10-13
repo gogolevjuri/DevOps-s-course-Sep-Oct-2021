cf8e1bafb2c90fbc80a203988d8f9ed30e4b463e
gogolevjuri <gogolevjuri@gmail.com>
2021-10-14 00:09:33 +0300

    working on setting file don't want share my telegram token... security...

--- telegrambot_ongolang/main.go
+++ telegrambot_ongolang/main.go
@@ -4,10 +4,19 @@ import (
 	"time"
 	tb "gopkg.in/tucnak/telebot.v2"
 	"github.com/spf13/viper"
+	"fmt"
 )
 
 func main() {
     conf := viper.New()
+    conf.SetConfigName("conf") // name of config file (without extension)
+    conf.SetConfigType("yaml") // REQUIRED if the config file does not have the extension in the name
+    conf.AddConfigPath("files")   // path to look for the config file in
+    conf.AddConfigPath(".")               // optionally look for config in the working directory
+    err := conf.ReadInConfig() // Find and read the config file
+    if err != nil { // Handle errors reading the config file
+    	panic(fmt.Errorf("Fatal error files/conf.yaml file: %w \n", err))
+    }
 
 	b, err := tb.NewBot(tb.Settings{
 		Token: "https://pkg.go.dev/gopkg.in/tucnak/telebot.v2",

d9296f05d30a0c26e0cb337c3115471f13e2a5c2
gogolevjuri <gogolevjuri@gmail.com>
2021-10-14 00:50:23 +0300

    Ok now viper reads config file and gets Token for telegram bot Also telegram bot gets all msg's and answer hello world

--- telegrambot_ongolang/main.go
+++ telegrambot_ongolang/main.go
@@ -1,25 +1,37 @@
 package main
 
 import (
-	"time"
-	tb "gopkg.in/tucnak/telebot.v2"
-	"github.com/spf13/viper"
 	"fmt"
+	"github.com/spf13/viper"
+	tb "gopkg.in/tucnak/telebot.v2"
+	"time"
 )
 
+type Data struct {
+	APITOKEN string
+}
+
 func main() {
-    conf := viper.New()
-    conf.SetConfigName("conf") // name of config file (without extension)
-    conf.SetConfigType("yaml") // REQUIRED if the config file does not have the extension in the name
-    conf.AddConfigPath("files")   // path to look for the config file in
-    conf.AddConfigPath(".")               // optionally look for config in the working directory
-    err := conf.ReadInConfig() // Find and read the config file
-    if err != nil { // Handle errors reading the config file
-    	panic(fmt.Errorf("Fatal error files/conf.yaml file: %w \n", err))
-    }
+	var configdata Data
+
+	conf := viper.New()
+	conf.SetConfigName("conf")  // name of config file (without extension)
+	conf.SetConfigType("yaml")  // REQUIRED if the config file does not have the extension in the name
+	conf.AddConfigPath("files") // path to look for the config file in
+	conf.AddConfigPath(".")     // optionally look for config in the working directory
+	err := conf.ReadInConfig()  // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error files/conf.yaml file: %w \n", err))
+	}
+	err = conf.Unmarshal(&configdata)
+	if err != nil {
+		fmt.Printf("Unable to decode into struct, %v", err)
+	}
+	APITOKEN := configdata.APITOKEN
+	fmt.Printf("test", APITOKEN)
 
 	b, err := tb.NewBot(tb.Settings{
-		Token: "https://pkg.go.dev/gopkg.in/tucnak/telebot.v2",
+		Token:  APITOKEN,
 		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
 	})
 

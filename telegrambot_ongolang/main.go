e65feb25c21b89fa1af668f170e29e24288eb424
gogolevjuri <gogolevjuri@gmail.com>
2021-10-13 23:52:56 +0300

    add viper lib and update gitignore

--- telegrambot_ongolang/main.go
+++ telegrambot_ongolang/main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"time"
 	tb "gopkg.in/tucnak/telebot.v2"
+	"github.com/spf13/viper"
 )
 
 func main() {
 	b, err := tb.NewBot(tb.Settings{
-		Token: "TOKEN_HERE",
+		Token: "https://pkg.go.dev/gopkg.in/tucnak/telebot.v2",
 		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
 	})
 
